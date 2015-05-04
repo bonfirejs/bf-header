b8ecb490c6f4df147a621eb69bf8ab4d0d4a724a
Alex Houchens <gregoryx.houchens@intel.com>
2015-05-04 10:43:17 -0700

    Allow for consuming app to define header appName

--- addon/components/bf-header.js
+++ addon/components/bf-header.js
@@ -1,9 +1,25 @@
+/**
+* @property appName is a computed property that first checks the consuming app config/environment.js
+* for APP.bonfire.title
+* APP:  {
+*  bonfire: {
+*    title: 'Your App Name'
+*  }
+* }
+* If this is not provided then it will fetch from application.namespace.name
+*/
+
 import Ember from 'ember';
 import layout from '../templates/components/bf-header';
 
 export default Ember.Component.extend({
   layout: layout,
   appName: function() {
-    return Ember.String.capitalize(this.get('application.namespace.name'));
+    var configTitle = this.container.lookupFactory('config:environment').APP.bonfire.title;
+    if (Ember.isEmpty(configTitle)) {
+      return Ember.String.capitalize(this.get('application.namespace.name'));
+    } else {
+      return configTitle;
+    }
   }.property('application.namespace.name')
 });
